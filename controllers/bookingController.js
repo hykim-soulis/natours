488427ec67f87066962312d20b09968cd6b754b4
hykim-soulis <hyeyoung9002@gmail.com>
2022-07-28 14:30:37 -0500

    minor booking update

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -13,10 +13,10 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
-    success_url: `//${req.get('host')}/?tour=${req.params.tourId}&user=${
-      req.user.id
-    }&price=${tour.price}`,
-    cancel_url: `//${req.get('host')}/tour/${tour.slug}`,
+    success_url: `${req.protocol}://${req.get('host')}/?tour=${
+      req.params.tourId
+    }&user=${req.user.id}&price=${tour.price}`,
+    cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
     line_items: [
@@ -30,7 +30,6 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
       },
     ],
   });
-  console.log('http🌽🌽', req);
 
   // 3) Create session as response
   res.status(200).json({
